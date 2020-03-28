493d21e2091fbf3a00e6ca3cd63cf47506bfd195
Lee Whelan <lee.whelan@leveris.com>
2020-03-28 22:00:31 +0000

    Added update User endpoint.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
 
   # GET /users/{id}
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   # POST /users
@@ -16,11 +17,24 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PATCH /users/{id}
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
-  
+
   # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
